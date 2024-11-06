79ceda7b04a76ae8bff4e6b277e73cc39c5eefb4
vladimirandropov <vvvvvvvvvv@inbox.ru>
2024-11-06 13:07:02 +0300

    add createcard

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,19 @@
 // @todo: Темплейт карточки
-const listContainer = document.querySelector('.cards');
+const listContainer = document.querySelector('.places__list');
 const template = document.querySelector('.template');
-
+//const listContainer = document.querySelector('.cards');
+//const template = document.querySelector('.template');
 // @todo: DOM узлы
+
 
 // @todo: Функция создания карточки
 
+
 // @todo: Функция удаления карточки
 
+
 // @todo: Вывести карточки на страницу
+
 
 function renderList() {
     const listCards = initialCards.map(composeItem);
@@ -18,19 +23,19 @@ function renderList() {
 
 function composeItem(item){
     const newItem = template.content.querySelector('.card').cloneNode(true);
-    const cardsImg = newItem.querySelector('.cards__img');
-    const cardsTitle = newItem.querySelector('.cards__title')
-    const cardsBtnRemove = newItem.querySelector('.cards__btn-remove');
-    const cardsLike = newItem.querySelector('.cards__like');
+    const cardsImg = newItem.querySelector('.card__img');
+    const cardsTitle = newItem.querySelector('.card__title')
+//    const cardsBtnRemove = newItem.querySelector('.card__delete-button');
+    const cardsLike = newItem.querySelector('.card__like-button');
  
     cardsImg.src = item.link;
     cardsImg.alt = item.name;
     cardsTitle.textContent = item.name;
  
-    cardsBtnRemove.addEventListener('click', removeItem);
+//    cardsBtnRemove.addEventListener('click', removeItem);
  
     cardsLike.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('cards__like_active');
+    evt.target.classList.toggle('card__like_active');
     });
  
     cardsImg.addEventListener('click', function(){

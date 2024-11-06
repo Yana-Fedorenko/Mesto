7c01b8c91b1bc9e5609c1767ad2cfd4e4e472709
vladimirandropov <vvvvvvvvvv@inbox.ru>
2024-11-06 14:36:05 +0300

    Crearecard

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,41 @@
 // @todo: Темплейт карточки
+// const listContainer = document.querySelector('.cards');
+// const template = document.querySelector('.template');
+const listContainer = document.querySelector('.places__list');
+const template = document.querySelector('.template');
 // @todo: DOM узлы
 // @todo: Функция создания карточки
 // @todo: Функция удаления карточки
 // @todo: Вывести карточки на страницу
+
+function renderList() {
+    const listCards = initialCards.map(createCard);
+     
+    listContainer.append(...listCards);
+}
+ 
+function createCard(item){
+    const newItem = template.content.querySelector('.card').cloneNode(true);
+    const cardsImg = newItem.querySelector('.card__image');
+    const cardsTitle = newItem.querySelector('.card__title')
+    const cardsBtnRemove = newItem.querySelector('card__delete-button');
+    const cardsLike = newItem.querySelector('.card__like-button');
+ 
+    cardsImg.src = item.link;
+    cardsImg.alt = item.name;
+    cardsTitle.textContent = item.name;
+ 
+    // cardsBtnRemove.addEventListener('click', removeItem);
+ 
+    cardsLike.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__like-button_active');
+    });
+ 
+    cardsImg.addEventListener('click', function(){
+    openImage(item);
+    });
+ 
+    return newItem;
+}
+
+renderList();
